5a4b927516e38705e8c341ba9392289530d736fb
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-09 20:34:40 +0100

    my-portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router , Routes , Route , Link} from 'react-router-dom'
+import "./App.scss"
+import Home from './Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <div className='navbar-title'>
+        <h1>&lt;div className=Maciej Wawryszuk /&gt;</h1>
+        </div>
+        <div className='navbar-navigation'>
+          <a href=''>Projects</a>
+          <a href=''>Skills</a>
+          <a href=''>Certyficates</a>
+          <a href=''>Education</a>
+          <a href=''>Home</a>
+        </div>
+       
+      </div>
+      <div className='pages'>
+      <div className='first-page'>
+          <div className='first-page-aboutme'>
+          <h2>HEY ! I,m Maciej</h2>  
+            <h3>In love with creating web applications as<br />
+                a Frontend Developer<br/>
+                using React / SASS / Javascript <br />
+                and other interesting frameworks and libraries</h3>
+          </div>
+          <div className='first-page-photo'>
+    
+          </div>
+        </div>
+        </div>
     </div>
   );
 }
