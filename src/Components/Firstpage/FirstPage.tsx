686ad9ecabdccaf13c5c898e4251cce36fbde825
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-10 21:20:49 +0100

    update

--- src/Components/Firstpage/FirstPage.tsx
+++ src/Components/Firstpage/FirstPage.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import profile from '../../assets/profile.jpg'
+import insta from '../../assets/instagram.png'
+import facebook from '../../assets/facebook.png'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import twiiter from '../../assets/twitter.png'
 const FirstPage = () => {
   return (
     <div>
@@ -11,11 +16,19 @@ const FirstPage = () => {
           using React / SASS / Javascript and<br />
           other interesting frameworks and libraries</h3>
     </div>
+    
     <div className='first-page-photo'>
       <img src={profile} className="profile-photo" />
     </div>
     <div className='first-page-contact-button'>
-      <a href="">Contact</a>
+      <a href="">Contact Me</a>
+    </div>
+    <div className='first-page-icons-links'>
+      <a href=""><img src={linkedin}/></a>
+      <a href=""><img src={github}/></a>
+      <a href=""><img src={facebook}/></a>
+      <a href=""><img src={twiiter}/></a>
+      <a href=""><img src={insta}/></a>
     </div>
   </div>
   </div>
