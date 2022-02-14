24227a1c3e6baf998edc8f0daf412a5dd31eba98
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-14 22:39:08 +0100

    :D

--- src/Components/FifthPage/FifthPage.tsx
+++ src/Components/FifthPage/FifthPage.tsx
@@ -7,6 +7,7 @@ const FifthPage = () => {
         <div className='fifth-page-title'>
          <h1>Projects</h1>
         </div>
+        <div className='fifth-page-container'>
         <div className='fifth-page-text'>
             <h3>Online Vapeshop</h3>
             <h3>WeatherApp</h3>
@@ -20,6 +21,7 @@ const FifthPage = () => {
             <img src={weatherapp} />
             
             <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/d1b29a23622187.563260170f313.jpg" />
+        </div>
         </div>
     </div>
   )
