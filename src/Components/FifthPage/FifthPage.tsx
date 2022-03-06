b6e8e9db5bc2f22f9f36461e37856ac4b001bc91
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-03-06 12:45:37 +0100

    update

--- src/Components/FifthPage/FifthPage.tsx
+++ src/Components/FifthPage/FifthPage.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import weatherapp from '../../assets/weatherapp.png'
-import vape from '../../assets/vapeshop.png'
+import React from "react";
+import weatherapp from "../../assets/weatherapp.png";
+import vape from "../../assets/vapeshop.png";
+import mysaving from "../../assets/mysavings.png";
+import four from "../../assets/4.png";
 const FifthPage = () => {
   return (
-    <div className='fifth-page' id="projects">
-        <div className='fifth-page-title'>
-         <h1>Projects</h1>
+    <div className="fifth-page" id="projects">
+      <div className="fifth-page-title">
+        <h1>Projects</h1>
+      </div>
+      <div className="fifth-page-container">
+        <div className="fifth-page-text">
+          <h3>Online Vapeshop</h3>
+          <h3>WeatherApp</h3>
+          <h3>MySaving</h3>
         </div>
-        <div className='fifth-page-container'>
-        <div className='fifth-page-text'>
-            <h3>Online Vapeshop</h3>
-            <h3>WeatherApp</h3>
-            <h3>MyFinance</h3>
-        </div>
-        
-        <div className='fifth-page-image'>
-          
-            <img src={vape} />
-            
-            <img src={weatherapp} />
-            
-            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/d1b29a23622187.563260170f313.jpg" />
-        </div>
-        </div>
-    </div>
-  )
-}
 
-export default FifthPage
+        <div className="fifth-page-image">
+          <a href="https://maciejj1.github.io/mysaving/">
+            <img src={mysaving} />
+          </a>
+
+          <a href="https://maciejj1.github.io/WeatherApp/">
+            <img src={weatherapp} />
+          </a>
+          <a href="https://maciejj1.github.io/CloudShop/">
+            <img src={vape} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FifthPage;
--- src/Components/SixthPage/SixthPage.tsx
+++ src/Components/SixthPage/SixthPage.tsx
@@ -1,30 +1,35 @@
-import React from 'react'
-import location from '../../assets/placeholder.png'
-import gmail from '../../assets/gmail.png'
-import twitter from '../../assets/twitter.png'
-import linkedin from '../../assets/linkedin.png'
+import React from "react";
+import location from "../../assets/placeholder.png";
+import gmail from "../../assets/gmail.png";
+import twitter from "../../assets/twitter.png";
+import linkedin from "../../assets/linkedin.png";
 const SixthPage = () => {
   return (
-    <div className='sixth-page' id="contact">
-     <div className='sixth-page-title'>
-      <h1>Contact Me!</h1>
-     </div>
-       <div className='sixth-page-text'>
-        <img src={location}/>
+    <div className="sixth-page" id="contact">
+      <div className="sixth-page-title">
+        <h1>Contact Me!</h1>
+      </div>
+      <div className="sixth-page-text">
+        <img src={location} />
         <h3>Lublin/Poland</h3>
-        
-       </div>
-       <div className='sixth-page-icons'>
-         <h3>Send me message on:</h3>
-        <a href="mailto:maciekwawryszuk@gmail.com"><img src={gmail} /> </a>
-        <a href="https://twitter.com/Maciej02191496"><img src={twitter} /></a>
-        <a href="https://www.linkedin.com/in/maciek-wawryszuk-484737225/"><img src={linkedin} /></a>
-       </div>
-       <div className='sixth-page-code-logo'>
-           <img src="https://cdn-icons-png.flaticon.com/512/59/59118.png" />
-       </div>
+      </div>
+      <div className="sixth-page-icons">
+        <h3>Send me message on:</h3>
+        <a>
+          <img src={gmail} /> <h2>maciekwawryszuk@gmail.com</h2>
+        </a>
+        <a href="https://twitter.com/Maciej02191496">
+          <img src={twitter} />
+        </a>
+        <a href="https://www.linkedin.com/in/maciek-wawryszuk-484737225">
+          <img src={linkedin} className="linkedin" />
+        </a>
+      </div>
+      <div className="sixth-page-code-logo">
+        <img src="https://cdn-icons-png.flaticon.com/512/59/59118.png" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SixthPage
+export default SixthPage;
